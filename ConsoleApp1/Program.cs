92d135d165db906db935f56aa1e5dab30be3bc01
student <student@DESKTOP-QTJ324R>
2022-09-09 21:15:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = new int[5]; //создание массива на 5 элементов
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите число для индекса {0}", i);
+                massive[i] = Int32.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
