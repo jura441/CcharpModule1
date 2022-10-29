a870e1a67533571415eb06d83791993c68fada88
Юрий Иванов <jura401@mail.ru>
2022-10-29 17:49:50 +0300

    2910

---
+++ Exam/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Program
+    {
+
+        string _name;
+        string _password;
+        DateTime _birthDate;
+        public string Name { get; set; }
+        public string Login { get => _name; set => _name = value; }
+        public string Password { get => _password; set => _password = value; }
+        public DateTime DateOfBirth { get => _birthDate; set => _birthDate = value; }
+        public string ToString();
+    }
+}
--- Lesson10.1/Program.cs
+++ Lesson10.1/Program.cs
@@ -8,82 +8,115 @@ using System.Timers;
 
 using System.Windows.Forms;
 
-//namespace Lesson10._1
-//{
-//    internal class Program
-//    {
-//            static System.Timers.Timer timer = new System.Timers.Timer();
-//            static Random rnd = new Random();
-//            static Dog dog = new Dog(0);
-//        static void Main(string[] args)
-//        {
-//            //dog.Calm();
-//            //dog.Sad();
-//            //dog.Wanna();
-//        }
-//        private void Timer_Elapsed(object? sender, System.ElapsedEventArgs e)
-//        {
-//            Timer.Interval = rnd.Next(1000, 10000);
-//            int action = rnd.Next(1, 6);
+namespace Lesson10._1
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static Dog dog = new Dog();
+        static System.Timers.Timer timer = new System.Timers.Timer(3000);
+        static DateTime dt;
+        static void Main(string[] args)
+        {
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+            dt = DateTime.Now;
+            dt = dt.AddMinutes(3);
+            Console.ReadLine();
+        }
+        static private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (!dog.LivesDog() && DateTime.Now < dt)
+            {
+                if (DateTime.Now > dt)
+                {
+                    timer.Stop();
+                    Console.WriteLine("Собака выжила в ваших руках.");
+                }
+                Console.WriteLine(e.SignalTime);
+                timer.Interval = rnd.Next(1000, 10000);
+                int action = rnd.Next(1, 6);
+                switch (action)
+                {
+                    case 1: dog.Wanna("Хочу играть"); break;
+                    case 2: dog.Wanna("Хочу есть"); break;
+                    case 3: dog.Wanna("Хочу спать"); break;
+                    case 4: dog.Wanna("Хочу гулять"); break;
+                    case 5: dog.Wanna("Хочу в туалет"); break;
+                    default: MessageBox.Show("Песика похитили"); break;
+                }
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+    }
 
-//        }
-//        class Dog
-//        {
-//            int _state = 0;
-//            int _live = 3;
-//            bool death = false;
+    class Dog
+    {
+        int _live = 3;
+        bool _death = false;
 
-//            public Dog(int state)
-//            {
-//                _state = state;
-//            }
-//            public string Wanna(string wish)
-//            {
-//                DialogResult dr = new DialogResult();
-//                dr = MessageBox.Show("ГАВ!" + wish, wish, MessageBoxButtons.OKCancel);
-//                    if (DialogResult.OK == dr)
-//                {
-//                    if (_live < 3)
-//                    {
-//                        _live++;
-//                    }
-//                }
-//                if (DialogResult.Cancel == dr)
-//                {
-//                    Console.Clear();
-//                    Sad();
-//                    _live--;
-//                }
-//                if(_live==0)
-//                {
-//                    Console.WriteLine("Песик умрет");
-//                }
-//            }
-//            public void Calm()
-//            {
-//                Console.WriteLine("   /\\____/\\  ");
-//                Console.WriteLine(" (           ) ");
-//                Console.WriteLine("(  /\\  /\\   )");
-//                Console.WriteLine("(  \\/  \\/   )");
-//                Console.WriteLine("(             )");
-//                Console.WriteLine("(      ()     )");
-//                Console.WriteLine("(    ____     )");
-//                Console.WriteLine(" (   |__|     )");
-//                Console.WriteLine("   (______)   )");
-//            }
+        public Dog()
+        {
 
-//            public void Sad()
-//            {
-//                Console.WriteLine("   /\\____/\\  ");
-//                Console.WriteLine(" (           ) ");
-//                Console.WriteLine("(  /\\  /\\   )");
-//                Console.WriteLine("(  \\/  \\/   )");
-//                Console.WriteLine("(   ..  ..    )");
-//                Console.WriteLine("(      ()     )");
-//                Console.WriteLine("(    ____     )");
-//                Console.WriteLine(" (   |__|     )");
-//                Console.WriteLine("   (______)   )");
-//            }
-//        }
-//    }
-//}
+        }
+        public bool LivesDog()
+        {
+            return _death;
+        }
+        public void Wanna(string wish)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("ГАВ! " + wish, wish, MessageBoxButtons.OKCancel);
+            if (DialogResult.OK == dr)
+            {
+                if (_live < 3)
+                {
+                    _live++;
+                }
+                Console.Clear();
+                Calm();
+                Console.WriteLine($"Песик имеет {_live} жизней");
+            }
+            if (DialogResult.Cancel == dr)
+            {
+                Console.Clear();
+                Sad();
+                _live--;
+                Console.WriteLine($"Песик имеет {_live} жизней");
+            }
+            if (_live == 0)
+            {
+                _death = true;
+                Console.WriteLine("Песик умер");
+            }
+        }
+        public void Calm()
+        {
+            Console.WriteLine("  /\\____/\\  ");
+            Console.WriteLine(" (          ) ");
+            Console.WriteLine("(  /\\  /\\   )");
+            Console.WriteLine("(  \\/  \\/   )");
+            Console.WriteLine("(           )");
+            Console.WriteLine("(     ()    )");
+            Console.WriteLine("(    ____   )");
+            Console.WriteLine(" (   |___| ) ");
+            Console.WriteLine("   (______)  ");
+        }
+
+        public void Sad()
+        {
+            Console.WriteLine("  /\\____/\\  ");
+            Console.WriteLine(" (          ) ");
+            Console.WriteLine("(  /\\  /\\   )");
+            Console.WriteLine("(  \\/  \\/   )");
+            Console.WriteLine("(    '   '   )");
+            Console.WriteLine("(     ()    )");
+            Console.WriteLine("(    ____   )");
+            Console.WriteLine(" (   |___| ) ");
+            Console.WriteLine("   (______)  ");
+        }
+    }
+}
